4aae1bbd4e09f48d992dbf66000e254fd60b6a65
aruasouza <arua.viso@gmail.com>
2023-01-17 13:44:02 -0300

    mudanças no montecarlo

--- montecarlo.py
+++ montecarlo.py
@@ -1,19 +1,44 @@
 import numpy as np
 import pandas as pd
-import random
 import os
 from azure.datalake.store import multithread
 from setup import adlsFileSystemClient
+from bcb import sgs,currency
+from datetime import date
 
-def distribution(media,std,minimo,maximo,n):
-    dist = list(np.random.normal(media,std,n))
-    for i,valor in enumerate(dist):
-        if not (minimo <= valor <= maximo):
-            dist[i] = (random.random() * (maximo - minimo)) + minimo
-    return dist
+def absolute(serie):
+    valor_atual = 1598.41
+    yield valor_atual
+    for valor in serie[1:]:
+        valor = valor / 100
+        valor_atual += (valor_atual * valor)
+        yield valor_atual
+
+def get_current_ipca():
+    try:
+        dataframe = sgs.get({'ipca_change':433},start = '2000-01-01')
+    except:
+        raise TimeoutError('Erro de conexão com o Banco Central')
+    dataframe['indice'] = [valor for valor in absolute(dataframe['ipca_change'].values)]
+    return dataframe['indice'].values[-1]
+
+def get_current_cambio():
+    try:
+        cy = currency.get('USD', start = '2022-01-01',end = str(date.today()))
+    except:
+        raise TimeoutError('Erro de conexão com o Banco Central')
+    return cy.values.ravel()[-1]
+
+def get_current_selic():
+    try:
+        dataframe = sgs.get({'selic':432}, start = '2022-01-01')
+    except:
+        raise TimeoutError('Erro de conexão com o Banco Central')
+    return dataframe.values.ravel()[-1]
 
 def find_files(risco):
     records = pd.read_csv('records.csv')
+    global contracts
     try:
         filtered_df = records.loc[[x.find(risco) != -1 for x in records['file_name']]].copy()
         file_names = filtered_df['file_name'].values[-10:]
@@ -37,15 +62,16 @@ def find_datas(index):
     main_dataframe['mes'] = main_dataframe['date'].apply(lambda x: x.split('-')[1])
     os.remove(file_name)
 
-def linhas_limite(data,simulation):
+def impact():
     pass
 
 def simulate(mes,ano):
-    data = f'{ano}-{mes}'
-    linha = main_dataframe.loc[main_dataframe['date'] == data]
-    media = linha['prediction'].iloc[0]
-    maximo = linha['superior'].iloc[0]
-    minimo = linha['inferior'].iloc[0]
-    std = main_dataframe.loc[0,'std']
+    data_final = f'{ano}-{mes}'
+    std = main_dataframe['std'].iloc[0]
+    df_pred = main_dataframe[['date','prediction']].copy()
+    df_pred = df_pred.set_index(pd.to_datetime(df_pred['date'],format = '%Y-%m'))
     global simulation
-    simulation = distribution(media,std,minimo,maximo,1000)
+    simulation = np.random.normal(size = 10000)
+    # contracts = pd.read_excel('Valores_cambio.xlsx')
+    # contracts = contracts.set_index(pd.to_datetime(contracts['data'].apply(lambda x: x[3:]),format = '%m/%Y'))
+    
