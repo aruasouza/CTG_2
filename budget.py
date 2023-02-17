68a6abaa7d32e8268d45c9b6e790e1316045aadd
aruasouza <arua.viso@gmail.com>
2023-02-17 11:38:22 -0300

    calculo cdi

--- budget.py
+++ budget.py
@@ -25,14 +25,49 @@ def upload_file(risco,tipo):
         rpath=f'{directory}/{risco}/{file_name}', nthreads=64, overwrite=True, buffersize=4194304, blocksize=4194304)
     os.remove(file_name)
 
+def composed_interest(days,values):
+    results = [values[0] + 1]
+    for day,valor in zip(days[1:],values[1:]):
+        if day == 0:
+            results.append(1)
+        else:
+            results.append(results[-1] * valor + 1)
+    return np.array(results) - 1
+
 def read_cash_dcf():
-    dcf = pd.read_excel("datalake_files/DCF - RP'22 8&04 - Valores.xlsx",sheet_name = 'Budget',header = 5)
-    dcf = dcf.loc[~dcf['Unnamed: 1'].isnull()].drop('Unnamed: 0',axis = 1).set_index('Unnamed: 1').T
-    dcf.columns.name = None
-    dcf = dcf[list(map(lambda x: type(x) == datetime.datetime,dcf.index.values))]
-    serie_selic = dcf[['Cash Accumulated Available']].rename({'Cash Accumulated Available':'Cash'},axis = 1) * 100
-    serie_selic = serie_selic.set_index(pd.DatetimeIndex(serie_selic.index))
-    return serie_selic[datetime.datetime.today():]
+    # dcf = pd.read_excel("datalake_files/DCF - RP'22 8&04 - Valores.xlsx",sheet_name = 'Budget',header = 5)
+    # dcf = dcf.loc[~dcf['Unnamed: 1'].isnull()].drop('Unnamed: 0',axis = 1).set_index('Unnamed: 1').T
+    # dcf.columns.name = None
+    # dcf = dcf[list(map(lambda x: type(x) == datetime.datetime,dcf.index.values))]
+    # serie_selic = dcf[['Cash Accumulated Available']].rename({'Cash Accumulated Available':'Cash'},axis = 1) * 100
+    # serie_selic = serie_selic.set_index(pd.DatetimeIndex(serie_selic.index))
+    # return serie_selic[datetime.datetime.today():]
+    arquivo = 'datalake_files/09 Debt vs Setembro 2022 - RP - Budget 2023.xlsx'
+    dfs = []
+    messages = []
+    for i in range(1,11):
+        try:
+            df = pd.read_excel(arquivo,sheet_name = f'{i}ª Emissão CDI',header = 4)
+            df = df[df['Filtro'] == 'X'][['Data','Last  CDI','Days','Interest (Month)','Principal Balance']].dropna()
+            df['Data'] = pd.to_datetime(df['Data'],format = '%d%m%Y')
+            if df['Data'].iloc[0] < datetime.datetime.now():
+                df['Last  CDI'] = df['Last  CDI'] / 100
+                spread = pd.read_excel(arquivo,sheet_name = '1ª Emissão CDI').iloc[2,9] / 100
+                df['Spread'] = spread
+                df['Taxa Diária Estimada'] = ((df['Last  CDI'] + spread + 1) ** (1/252)) - 1
+                df['Days Dif'] = df['Days'].diff().fillna(df['Days'].iloc[0]).apply(lambda x: 0 if x < 2 else x)
+                df['Taxa Estimada Período'] = ((1 + df['Taxa Diária Estimada']) ** df['Days Dif']) - 1
+                df['Taxa Acumulada Estimada'] = composed_interest(df['Days Dif'].values,df['Taxa Estimada Período'].values)
+                df['Juros Estimados'] = df['Principal Balance'] * df['Taxa Acumulada Estimada']
+                df['Período'] = df['Data'].apply(lambda x: str(x.year) + '-' + str(x.month))
+                dfs.append(df)
+                messages.append(f'{i}ª Emissão CDI: Sucesso')
+            else:
+                messages.append(f'{i}ª Emissão CDI: Não Realizado')
+        except ValueError as e:
+            print(e)
+            messages.append(f'{i}ª Emissão CDI: ' + str(e))
+    return dfs
 
 def read_rp():
     rp = pd.read_excel('datalake_files/09 Debt vs Setembro 2022 - RP - Budget 2023.xlsx',sheet_name = 'Daily Calculation prop2019',header = 2)
@@ -56,16 +91,50 @@ def shuffle(arr):
     np.random.shuffle(new)
     return new
 
+def sum_series(*args):
+    return pd.concat(args,axis = 1).sum(axis = 1)
+
 def risco_juros(selic,dcf):
     size = 10000
-    dcf['Date'] = dcf.index.to_period('m')
-    dcf['Date'] = dcf['Date'].apply(str)
-    risco = dcf.copy()
+    selic['Período'] = selic['date'].apply(lambda x: x[:5] + str(int(x[5:])))
+    selic = selic.set_index('Período')
+    first_date = pd.to_datetime(selic['date'].iloc[0],format = '%Y-%m')
+    # dcf['Date'] = dcf.index.to_period('m')
+    # dcf['Date'] = dcf['Date'].apply(str)
+    # risco = dcf.copy()
+    # risco = risco.join(selic.set_index('date')[['prediction']],on = 'Date')
+    # return cen_df.apply(lambda x: pd.Series(shuffle(x.values),index = x.index),axis = 1).cumsum()
     std = selic['std'].iloc[0]
-    risco = risco.join(selic.set_index('date')[['prediction']],on = 'Date')
-    simulation = np.random.normal(size = size,scale = std)
-    cen_df = pd.concat([(risco['prediction'] + sim) * risco['Cash'] for sim in simulation],axis = 1,names = list(range(size)))
-    return cen_df.apply(lambda x: pd.Series(shuffle(x.values),index = x.index),axis = 1).cumsum()
+    cen_df = pd.concat([(selic['prediction'].rename(i) + np.random.normal(scale = std,size = len(selic))) for i in range(size)],axis = 1,names = list(range(size)))
+    dfs = dcf
+    for i,df in enumerate(dfs):
+        df = df[df['Data'] >= first_date].copy()
+        df['Despesa Estimada'] = df['Juros Estimados'].cumsum()
+        df['Despesa Real'] = (df['Interest (Month)'] * df['Days Dif'].apply(lambda x: 0 if x == 0 else 1)).cumsum()
+        df['Erro'] = (df['Despesa Real'] - df['Despesa Estimada']) / df['Despesa Real']
+        dfs[i] = df
+    cenarios = []
+    for col in cen_df.columns:
+        cenario = cen_df[[col]] / 100
+        parciais = []
+        for df in dfs:
+            temp = df.join(cenario,on = 'Período')
+            temp['Taxa Anual'] = ((temp[col] + 1) ** 12) - 1 + temp['Spread']
+            temp['Taxa Diária Estimada'] = ((temp['Taxa Anual'] + 1) ** (1/252)) - 1
+            temp['Taxa Estimada Período'] = ((1 + temp['Taxa Diária Estimada']) ** temp['Days Dif']) - 1
+            temp['Taxa Acumulada Estimada'] = composed_interest(temp['Days Dif'].values,temp['Taxa Estimada Período'].values)
+            temp['Juros Estimados'] = temp['Principal Balance'] * temp['Taxa Acumulada Estimada']
+            temp['Despesa Estimada'] = temp['Juros Estimados'].cumsum()
+            temp['Despesa Corrigida'] = temp['Despesa Estimada'] * (1 - (temp['Erro']))
+            temp['Diferença Despesa'] = temp['Despesa Real'] - temp['Despesa Corrigida']
+            temp = temp[temp['Days Dif'] == 0].drop_duplicates('Período')
+            cenario_parcial = pd.Series(temp['Diferença Despesa'].values,index = temp['Período'].values)
+            parciais.append(cenario_parcial)
+        cenarios.append(sum_series(*parciais))
+        if len(cenarios) % 100 == 0:
+            print(len(cenarios))
+    final = pd.concat(cenarios,axis = 1)
+    return final.set_index(pd.to_datetime(final.index,format = '%Y-%m'))
 
 def risco_cambio(cambio,rp):
     size = 10000
